e53e6fbc30ac5057c5cf40b70ac3ca4889eef1a4
Samarth <samarthhegde79@gmail.com>
2023-08-17 17:02:15 +0530

    working with buttons

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+let color = "black";
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
 function createBoard(size){
@@ -11,7 +17,40 @@ function createBoard(size){
     
     for (let i = 0; i<numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover",colorDiv)
         board.insertAdjacentElement("beforeend", div);
     }
 }   
+
+function getSize(){
+    let input = prompt("Whats the size of grids you want?");
+    let message = document.querySelector(".message");
+    if(input == ""){
+        message.innerHTML = "please provide a number";  
+    }
+    else if(input<=0 || input> 100){
+        message.innerHTML = "please provide number between 1 and 100";
+    }
+    else{
+        message.innerHTML = "now you can play!";
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+    }
+    else{
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor(colorChoice){
+     color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+}
